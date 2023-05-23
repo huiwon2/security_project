5ed361b23158436c662f038818637927b54679d9
Leehuiwon <snowman4453@gmail.com>
2023-05-23 17:19:48 +0900

    [CREATE] create.html, KeyController.java, validate.html, ValidationController.java

---
+++ src/main/java/com/example/security_project/controller/ValidationController.java
@@ -0,0 +1,11 @@
+package com.example.security_project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class ValidationController {
+    @RequestMapping("/validate")
+    public String Validate(){
+        return "";
+    }
+}
