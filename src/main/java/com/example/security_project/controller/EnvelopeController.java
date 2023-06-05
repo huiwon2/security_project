9c917ce3297dc1a25d3a114b0f78c6a83acb9d40
Leehuiwon <snowman4453@gmail.com>
2023-06-06 03:45:17 +0900

    [CHORE] EnvelopeController.java, MainController.java, KeyController.java

--- src/main/java/com/example/security_project/controller/EnvelopeController.java
+++ src/main/java/com/example/security_project/controller/EnvelopeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class EnvelopeController {
-    @RequestMapping("/create")
+    @RequestMapping("/envelope")
     public String Input(){
         return "";
     }
